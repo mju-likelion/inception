a28823782e56ea11130ba8f0cf9f5e6133c92505
yhyem <yoonhyemin0120@gmail.com>
2023-06-19 00:42:40 +0900

    setting: globalStyle theme 색상코드 세팅

--- src/globalStyle/theme.ts
+++ src/globalStyle/theme.ts
@@ -4,6 +4,14 @@ import { DefaultTheme } from 'styled-components';
 export type Colors = {
   black: string;
   white: string;
+  mint1: string;
+  mint2: string;
+  red: string;
+  gray1: string;
+  gray2: string;
+  gray3: string;
+  gray4: string;
+  gray5: string;
 };
 
 /** @Note theme에 size요소를 추가한다면 여기에 타입을 추가해주세요 */
@@ -16,6 +24,14 @@ export const theme: DefaultTheme = {
   colors: {
     black: '#000',
     white: '#fff',
+    mint1: '#50E3C0',
+    mint2: '#C1FBED',
+    red: '#FF6E65',
+    gray1: '#656669',
+    gray2: '#8E8E93',
+    gray3: '#BBBBBD',
+    gray4: '#E5E5EA',
+    gray5: '#F2F2F5',
   },
   size: {
     pc: '1920px',
