a97feab725edc3e601b7f86f14cc65ee5cc1393d
mywhzy <cmj2393a@naver.com>
2023-06-20 05:01:34 +0900

    feat: color props 추가 chore: 파일명 파스칼 케이스로 변경

--- src/component/@share/atom/title.tsx
+++ src/component/@share/atom/title.tsx
@@ -3,14 +3,24 @@ import { styled, css } from 'styled-components';
 interface Props {
   ag: string;
   text: string;
+  color?: 'mint1' | 'gray3' | 'gray1' | 'black'; // string대신 리터럴 타입으로 명확히해주고 싶었는데 이게 더 확장성면에서 별로일까용,,?
 }
 
 /** @Note Figma 기준 Typography가 Title인 컴포넌트를 다룹니다.  */
-export const Title = ({ ag, text }: Props) => {
-  return <Text ag={ag}>{text}</Text>;
+export const Title = ({ ag, text, color }: Props) => {
+  return (
+    <Text ag={ag} color={color}>
+      {text}
+    </Text>
+  );
 };
 
-const Text = styled.h1<{ ag: string }>`
+const Text = styled.h1<{
+  ag: string;
+  color?: 'mint1' | 'gray3' | 'gray1' | 'black';
+}>`
+  color: ${({ theme, color = 'black' }) => theme.colors[color]};
+
   ${(props) =>
     props.ag === 'Title1' &&
     css`
