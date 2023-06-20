5988a6b86d8185cb14bc104855f5454182c9d74a
Bhanjo <hanjo1515@naver.com>
2023-06-21 02:33:35 +0900

    chore: 캘린더 헤더 스타일링

--- src/component/calendar/molecules/CalendarHeader.tsx
+++ src/component/calendar/molecules/CalendarHeader.tsx
@@ -1,9 +1,37 @@
 import { Title } from '@/component/@share';
+import { styled } from 'styled-components';
+import { ReactComponent as Back } from '@/assets/images/Back.svg';
+import { ReactComponent as Next } from '@/assets/images/Next.svg';
 
 export const CalendarHeader = () => {
   return (
-    <>
-      <Title ag="Title2" text="2023년 6월" />
-    </>
+    <Header>
+      <Title ag="Title2" text="2023년 6월" color="gray1" />
+      <ButtonWrapper>
+        <Back />
+        <Next />
+      </ButtonWrapper>
+    </Header>
   );
 };
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 52px;
+  h1 {
+    width: 180px;
+    /* background-color: red; */
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 70px;
+  justify-content: space-between;
+`;
--- src/component/calendar/organisms/calendar.tsx
+++ src/component/calendar/organisms/calendar.tsx
@@ -1,9 +1,44 @@
 import { CalendarHeader } from '@/component/calendar/molecules';
+import { getCalendarData } from '@/util';
+import { styled } from 'styled-components';
 
 export const Calendar = () => {
+  const date = getCalendarData('2023', '6');
   return (
-    <>
+    <Grid>
       <CalendarHeader />
-    </>
+      <GridItem>SUN</GridItem>
+      <GridItem>MON</GridItem>
+      <GridItem>TUE</GridItem>
+      <GridItem>WED</GridItem>
+      <GridItem>THU</GridItem>
+      <GridItem>FRI</GridItem>
+      <GridItem>SAT</GridItem>
+      <GridItem>B</GridItem>
+      <GridItem>C</GridItem>
+      <GridItem>D</GridItem>
+      <GridItem>E</GridItem>
+    </Grid>
   );
 };
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 40px);
+  grid-template-rows: 54px;
+
+  width: 320px;
+  height: 370px;
+  border: 1px solid red;
+  gap: 6px;
+  :nth-child(1) {
+    grid-column-start: 1;
+    grid-column-end: 8;
+    height: 54px;
+  }
+`;
+
+const GridItem = styled.div`
+  width: 40px;
+  border: 1px solid black;
+`;
