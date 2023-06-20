a261988fa08fe1caac2bfc75d8471bf567a0b6bf
Bhanjo <hanjo1515@naver.com>
2023-06-21 02:28:25 +0900

    feat: title 컴포넌트 color 지정 기능 추가

--- src/component/@share/atom/Title.tsx
+++ src/component/@share/atom/Title.tsx
@@ -1,16 +1,23 @@
+import { theme } from '@/globalStyle';
 import { styled, css } from 'styled-components';
 
 interface Props {
   ag: string;
   text: string;
+  color?: keyof typeof theme.colors;
 }
 
 /** @Note Figma 기준 Typography가 Title인 컴포넌트를 다룹니다.  */
-export const Title = ({ ag, text }: Props) => {
-  return <Text ag={ag}>{text}</Text>;
+export const Title = ({ ag, text, color = 'black' }: Props) => {
+  return (
+    <Text ag={ag} color={color}>
+      {text}
+    </Text>
+  );
 };
 
-const Text = styled.h1<{ ag: string }>`
+export const Text = styled.h1<{ ag: string; color: keyof typeof theme.colors }>`
+  color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
   ${(props) =>
     props.ag === 'Title1' &&
     css`
