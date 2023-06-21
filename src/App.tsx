b32d15799a12afde7a40dae7072abdeb9cb209d0
yhyem <yoonhyemin0120@gmail.com>
2023-06-22 03:03:56 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components';
 import { InitStyle, theme } from '@/globalStyle';
 import { Outlet } from 'react-router-dom';
 import GlobalFont from './globalStyle/GlobalFont';
-import { ButtonLarge } from './component/@share';
 
 /** @Note 모든 페이지에 적용할 설정 및 컴포넌트를 지정한다. */
 export function App() {
@@ -13,7 +12,6 @@ export function App() {
       <ThemeProvider theme={theme}>
         {/* 여기에 header, footer를 추가할 수 있다. */}
         {/* <Header /> */}
-        <ButtonLarge>노잼</ButtonLarge>
         {/* 라우터들이 Outlet에서 작동한다. */}
         <Outlet />
       </ThemeProvider>
