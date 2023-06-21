49c1be112c841f7c1030f0f18fddcbd48adcb0dc
yhyem <yoonhyemin0120@gmail.com>
2023-06-22 01:04:42 +0900

    fix: typographies css 수정

--- src/globalStyle/typographies.tsx
+++ src/globalStyle/typographies.tsx
@@ -1,3 +1,5 @@
+import { css } from 'styled-components';
+
 const weight = {
   Regular: 400,
   Semibold: 500,
@@ -16,51 +18,51 @@ export type FontType = {
 };
 
 export const typographies: FontType = {
-  Title1: {
-    'font-size': '26px',
-    'font-weight': weight.Bold,
-    'line-height': '28px',
-  },
-  Title2: {
-    'font-size': '20px',
-    'font-weight': weight.Bold,
-    'line-height': '24px',
-  },
-  Subtitle: {
-    'font-size': '18px',
-    'font-weight': weight.Semibold,
-    'line-height': '24px',
-  },
+  Title1: css`
+    font-size: '26px';
+    font-weight: ${weight.Bold};
+    line-height: '28px';
+  `,
+  Title2: css`
+    font-size: '20px';
+    font-weight: ${weight.Bold};
+    line-height: '24px';
+  `,
+  Subtitle: css`
+    font-size: '18px';
+    font-weight: ${weight.Semibold};
+    line-height: '28px';
+  `,
   Body1: {
-    Regular: {
-      'font-size': '16px',
-      'font-weight': weight.Semibold,
-      'line-height': '20px',
-    },
-    Semibold: {
-      'font-size': '16px',
-      'font-weight': weight.Semibold,
-      'line-height': '20px',
-    },
+    Regular: css`
+      font-size: '16px';
+      font-weight: ${weight.Regular};
+      line-height: '20px';
+    `,
+    Semibold: css`
+      font-size: '16px';
+      font-weight: ${weight.Semibold};
+      line-height: '20px';
+    `,
   },
-  Body2: {
-    'font-size': '15px',
-    'font-weight': weight.Regular,
-    'line-height': '22px',
-  },
-  Body3: {
-    'font-size': '14px',
-    'font-weight': weight.Regular,
-    'line-height': '18px',
-  },
-  Body4: {
-    'font-size': '12px',
-    'font-weight': weight.Semibold,
-    'line-height': '16px',
-  },
-  Body5: {
-    'font-size': '11px',
-    'font-weight': weight.Regular,
-    'line-height': '12px',
-  },
+  Body2: css`
+    font-size: '15px';
+    font-weight: ${weight.Regular};
+    line-height: '22px';
+  `,
+  Body3: css`
+    font-size: '14px';
+    font-weight: ${weight.Regular};
+    line-height: '18px';
+  `,
+  Body4: css`
+    font-size: '12px';
+    font-weight: ${weight.Semibold};
+    line-height: '16px';
+  `,
+  Body5: css`
+    font-size: '11px';
+    font-weight: ${weight.Regular};
+    line-height: '12px';
+  `,
 };
