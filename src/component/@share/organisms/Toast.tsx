52b9dd68c7f580a5e7e8337de082375d41799c42
mywhzy <cmj2393a@naver.com>
2023-06-23 13:56:34 +0900

    feat: Toast Component 구현

---
+++ src/component/@share/organisms/Toast.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { ReactComponent as SuccessIcon } from '@/assets/images/Success.svg';
+import { ReactComponent as ErrorIcon } from '@/assets/images/Error.svg';
+import { Body } from '@/component/@share/atom';
+
+type ToastProps = {
+  type: 'success' | 'error';
+  successMsg: string;
+  errorMsg: string;
+};
+
+export const Toast = ({ type, successMsg, errorMsg }: ToastProps) => {
+  return (
+    <Container>
+      {type === 'success' ? <SuccessIcon /> : <ErrorIcon />}
+      <MessageBox>
+        <Body ag="Body1" color="white">
+          {type === 'success' ? successMsg : errorMsg}
+        </Body>
+        {type === 'error' && (
+          <Body ag="Body3" color="gray3">
+            잠시 후 다시 시도해주세요
+          </Body>
+        )}
+      </MessageBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 320px;
+  height: 70px;
+  background-color: ${({ theme }) => theme.colors.gray1};
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  padding: 18px 12px;
+  margin: auto;
+  gap: 12px;
+`;
+
+const MessageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
