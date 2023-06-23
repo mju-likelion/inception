63b15e42e4d4c3f5f2da06d6a72cb605d8d3c839
yhyem <yoonhyemin0120@gmail.com>
2023-06-23 14:02:42 +0900

    chore: subTitle ag props 제거 chore: ButtonLarge 타이포 그래피 SubTitle로 수정

--- src/component/@share/atom/SubTitle.tsx
+++ src/component/@share/atom/SubTitle.tsx
@@ -3,33 +3,29 @@ import { theme } from '@/globalStyle';
 import { styled, css } from 'styled-components';
 
 interface Props {
-  ag: string;
   color?: keyof typeof theme.colors;
   align?: string;
 }
 
 /** @Note Figma 기준 Typography가 subTitle인 컴포넌트를 다룹니다.  */
 export const SubTitle = ({
-  ag,
   color = 'black',
   children,
   align = 'left',
 }: PropsWithChildren<Props>) => {
   return (
-    <Text ag={ag} color={color} align={align}>
+    <Text color={color} align={align}>
       {children}
     </Text>
   );
 };
 
 const Text = styled.p<{
-  ag: string;
   color: keyof typeof theme.colors;
   align: string;
 }>`
   color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
   ${(props) =>
-    props.ag === 'SubTitle' &&
     css`
       ${({ theme }) => theme.typographies.subTitle};
       letter-spacing: 0;
