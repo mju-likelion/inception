6ec573fd68abeb84d07fa0bfc7520e41dcae5929
mywhzy <cmj2393a@naver.com>
2023-06-23 13:53:53 +0900

    feat: Body 컴포넌트 Body1 Body3 스타일 추가

--- src/component/@share/atom/Body.tsx
+++ src/component/@share/atom/Body.tsx
@@ -5,18 +5,45 @@ interface Props {
   ag: string;
   children: React.ReactNode;
   color?: keyof typeof theme.colors;
+  body1Weight?: 'regular' | 'semiBold';
 }
 
-export const Body = ({ ag, children, color = 'black' }: Props) => {
+export const Body = ({
+  ag,
+  children,
+  color = 'black',
+  body1Weight = 'regular',
+}: Props) => {
   return (
-    <Text ag={ag} color={color}>
+    <Text ag={ag} color={color} body1Weight={body1Weight}>
       {children}
     </Text>
   );
 };
 
-const Text = styled.p<{ ag: string; color: keyof typeof theme.colors }>`
+const Text = styled.p<{
+  ag: string;
+  color: keyof typeof theme.colors;
+  body1Weight: 'regular' | 'semiBold';
+}>`
   color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
+  ${({ ag, body1Weight }) =>
+    ag === 'Body1' &&
+    css`
+      ${({ theme }) =>
+        body1Weight === 'regular'
+          ? theme.typographies.body1.regular
+          : theme.typographies.body1.semiBold}
+    `}
+
+  ${({ ag }) =>
+    ag === 'Body3' &&
+    css`
+      ${({ theme }) => theme.typographies.body3};
+      letter-spacing: 0px;
+    `}
+
+
   ${({ ag }) =>
     ag === 'Body4' &&
     css`
