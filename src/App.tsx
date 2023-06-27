63c4d7e85284b438a8ca2e1443a6774b0d61da82
yhyem <yoonhyemin0120@gmail.com>
2023-06-27 12:00:36 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components';
 import { InitStyle, theme, GlobalFont } from '@/globalStyle';
 import { Outlet } from 'react-router-dom';
 import { Header } from '@/component/@share/organisms';
-import { HelperModal } from './component/@share/organisms/HelperModal';
 
 /**@Note 모든 페이지에 적용할 설정 및 컴포넌트를 지정한다. */
 export function App() {
@@ -13,11 +12,6 @@ export function App() {
       <ThemeProvider theme={theme}>
         {/* 여기에 header, footer를 추가할 수 있다. */}
         <Header />
-        <HelperModal step={0} />
-        <HelperModal step={1} />
-        <HelperModal step={2} />
-        <HelperModal step={3} />
-
         {/* 라우터들이 Outlet에서 작동한다. */}
         <Outlet />
       </ThemeProvider>
