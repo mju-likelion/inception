26c6cda78c320825a126855896fe8042dbdc80b1
Sujin-Hasse <2017104858@khu.ac.kr>
2023-06-28 16:15:49 +0900

    chore: app 내부 테스트코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components';
 import { InitStyle, theme, GlobalFont } from '@/globalStyle';
 import { Outlet } from 'react-router-dom';
 import { Header } from '@/component/@share/organisms';
-import { Back } from './component/@share/atom/Back';
 
 /**@Note 모든 페이지에 적용할 설정 및 컴포넌트를 지정한다. */
 export function App() {
