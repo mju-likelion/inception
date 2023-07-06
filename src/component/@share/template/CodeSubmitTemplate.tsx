92491610fd53a55677f41563ff3ea6c34a03b032
Sujin-Hasse <2017104858@khu.ac.kr>
2023-07-07 02:34:12 +0900

    feat:  탭바 클릭에 따른 활성화 구현

--- src/component/@share/template/CodeSubmitTemplate.tsx
+++ src/component/@share/template/CodeSubmitTemplate.tsx
@@ -4,25 +4,31 @@ import { TitleBox } from '@/component/@share/molecules';
 import { Input } from '@/component/@share/atom';
 import { ButtonLarge } from '@/component/@share/atom';
 import { useState } from 'react';
+import { tabState } from '@/store';
+import { useRecoilState } from 'recoil';
 
 export const CodeSubmitTemlplate = () => {
+  const [memo, setMemo] = useState('');
+
   const onChange = (event: any) => {
     setMemo(event.target.value);
   };
 
-  const handleTabBar = () => {
-    console.log('클릭된다!!');
-    setClickTabBar(true);
-  };
+  const [clickTabBar, setClickTabBar] = useRecoilState(tabState);
 
-  const [memo, setMemo] = useState('');
-  const [clickTabBar, setClickTabBar] = useState(false);
+  const tab = () => {
+    console.log(clickTabBar);
+    //2. 어떤 버튼을 클릭했는지 판단해줘.
+    //디폴트버튼을 눌렀으면 default,결과버튼을 눌렀으면 result 판단.
+
+    setClickTabBar('result');
+  }; // 그리고 클릭한 버튼에 맞게 atom 의 상태를 바꿔줘};
 
   return (
     <>
       <TabBar
-        selectedTab={'result'}
-        onClick={handleTabBar}
+        selectedTab={clickTabBar} // 3. 탭바의 어떤버튼을 클릭하냐에 따라 이 selectTab의 값이 바뀌어야 함. 따라서 위에서 바꾼 아톰의 값을 줘
+        onClick={tab} // 1. 버튼을 클릭하면 tab 함수를 실행해죠
         firstTabTitle={'약속 잡기'}
         secondTabTitle={'결과 보기'}
         // 탭바를 누르면 - ---- 되고, true면 결과보기/false면 약속잡기 활성화
