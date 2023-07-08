b912f41f0df76fd76498457c30b63f849862b384
yhyem <yoonhyemin0120@gmail.com>
2023-07-09 04:04:41 +0900

    refactor: DuringDate startDate, endDate Date 인스턴스 생성 랜더링시 한번만 만들게 수정

--- src/component/timeBlock/atoms/DuringDate.tsx
+++ src/component/timeBlock/atoms/DuringDate.tsx
@@ -6,12 +6,14 @@ interface DuringDateProps {
 }
 
 export const DuringDate = ({ start, end }: DuringDateProps) => {
-  const startDate =
-    new Date(start).getMonth() + 1 + '/' + new Date(start).getDate();
-  const endDAte = new Date(end).getMonth() + 1 + '/' + new Date(end).getDate();
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  const stringStartDate = startDate.getMonth() + 1 + '/' + startDate.getDate();
+  const stringEndDate = endDate.getMonth() + 1 + '/' + endDate.getDate();
   return (
     <Body ag="Body4" color="gray2" align="center">
-      {startDate} - {endDAte}
+      {stringStartDate} - {stringEndDate}
     </Body>
   );
 };
