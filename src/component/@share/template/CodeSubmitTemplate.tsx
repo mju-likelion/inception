d342a95d4cae73b177ca2005a8c60aaa8e5ff98c
Sujin-Hasse <2017104858@khu.ac.kr>
2023-07-12 02:52:21 +0900

    chore: git conflict 해결

--- src/component/@share/template/CodeSubmitTemplate.tsx
+++ src/component/@share/template/CodeSubmitTemplate.tsx
@@ -10,7 +10,6 @@ import { useRecoilState } from 'recoil';
 export const CodeSubmitTemlplate = () => {
   const [value, setValue] = useState('');
 
-<<<<<<< HEAD
   const [buttonActive, setButtonActive] = useState(true);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,46 +33,11 @@ export const CodeSubmitTemlplate = () => {
     setSelectedTab(tab); // 그리고 클릭한 버튼에 맞게 atom 의 상태를 바꿔줘};
     setClickTabBar(tab);
   };
-=======
-  const onChange = (event: any) => {
-    setMemo(event.target.value);
-  };
-
-  const [clickTabBar, setClickTabBar] = useRecoilState(tabState);
-  const [selectedTab, setSelectedTab] = useState<'default' | 'result'>(
-    'default'
-  );
-
-<<<<<<< HEAD
-  const tab = () => {
-    console.log(clickTabBar);
-    //2. 어떤 버튼을 클릭했는지 판단해줘.
-    //디폴트버튼을 눌렀으면 default,결과버튼을 눌렀으면 result 판단.
-
-    setClickTabBar('result');
-  }; // 그리고 클릭한 버튼에 맞게 atom 의 상태를 바꿔줘};
->>>>>>> 6e3c9db (feat:  탭바 클릭에 따른 활성화 구현)
-=======
-  const tab = (tab: 'default' | 'result') => {
-    console.log(clickTabBar); //2. 어떤 버튼을 클릭했는지 판단해줘(디폴트버튼을 눌렀으면 default,결과버튼을 눌렀으면 result 판단.)
-    setSelectedTab(tab); // 그리고 클릭한 버튼에 맞게 atom 의 상태를 바꿔줘};
-    setClickTabBar(tab);
-  };
->>>>>>> 46bc8f3 (feat: 탭바 클릭 시 탭 active)
 
   return (
     <>
       <TabBar
-<<<<<<< HEAD
-<<<<<<< HEAD
         onClick={changeTabBar} // 1. 버튼을 클릭하면 tab 함수를 실행해죠. 어떤버튼을 클릭하냐에 따라 값이 바뀐 아톰을 줘
-=======
-        selectedTab={clickTabBar} // 3. 탭바의 어떤버튼을 클릭하냐에 따라 이 selectTab의 값이 바뀌어야 함. 따라서 위에서 바꾼 아톰의 값을 줘
-        onClick={tab} // 1. 버튼을 클릭하면 tab 함수를 실행해죠
->>>>>>> 6e3c9db (feat:  탭바 클릭에 따른 활성화 구현)
-=======
-        onClick={tab} // 1. 버튼을 클릭하면 tab 함수를 실행해죠. 어떤버튼을 클릭하냐에 따라 값이 바뀐 아톰을 줘
->>>>>>> 46bc8f3 (feat: 탭바 클릭 시 탭 active)
         firstTabTitle={'약속 잡기'}
         secondTabTitle={'결과 보기'}
       />
