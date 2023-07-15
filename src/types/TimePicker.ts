a010fba189f3eda4ae524840cfba2e39e5b8779f
mywhzy <cmj2393a@naver.com>
2023-07-16 03:56:21 +0900

    feat: TimeList as const 변경에 따른 type 수정

--- src/types/TimePicker.ts
+++ src/types/TimePicker.ts
@@ -1,3 +1,3 @@
 import { TIME_LIST } from '@/component/TimePicker/data';
 
-export type TimeListData = keyof typeof TIME_LIST;
+export type TimeListData = (typeof TIME_LIST)[keyof typeof TIME_LIST];
