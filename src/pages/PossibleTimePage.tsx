b5f04e9e46e59678c7ee9675bccbf92c2018509d
yhyem <yoonhyemin0120@gmail.com>
2023-07-15 22:02:34 +0900

    feat: selectTime 페이지 tabBar 추가

--- src/pages/PossibleTimePage.tsx
+++ src/pages/PossibleTimePage.tsx
@@ -4,29 +4,40 @@ import calendar from '@/assets/images/Calendar.svg';
 import { ButtonLarge } from '@/component/@share';
 import { styled } from 'styled-components';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { tabState } from '@/store';
+import { TabBar } from '@/component/@share';
 
 export const PossibleTimePage = () => {
   const [isActive, setIsActive] = useState(true);
+  const [tab, setTab] = useRecoilState(tabState);
 
   return (
-    <TimePageBlock>
-      <ContentBlock>
-        <TitleBoxBlock>
-          <TitleBox content="가용한 날짜들을 선택해주세요" step={2} />
-        </TitleBoxBlock>
-        <TimeBox onSetActiveButton={setIsActive} />
-        <InformationBlock>
-          <Information
-            icon={calendar}
-            title="선택 가능 시간"
-            content="09:00 - 17:00"
-          />
-        </InformationBlock>
-        <ButtonBlock>
-          <ButtonLarge isDisabled={isActive}>다음으로</ButtonLarge>
-        </ButtonBlock>
-      </ContentBlock>
-    </TimePageBlock>
+    <>
+      <TabBar
+        onClick={(tab: 'default' | 'result') => setTab(tab)}
+        firstTabTitle="약속 잡기"
+        secondTabTitle="결과 보기"
+      />
+      <TimePageBlock>
+        <ContentBlock>
+          <TitleBoxBlock>
+            <TitleBox content="가용한 날짜들을 선택해주세요" step={2} />
+          </TitleBoxBlock>
+          <TimeBox onSetActiveButton={setIsActive} />
+          <InformationBlock>
+            <Information
+              icon={calendar}
+              title="선택 가능 시간"
+              content="09:00 - 17:00"
+            />
+          </InformationBlock>
+          <ButtonBlock>
+            <ButtonLarge isDisabled={isActive}>다음으로</ButtonLarge>
+          </ButtonBlock>
+        </ContentBlock>
+      </TimePageBlock>
+    </>
   );
 };
 
