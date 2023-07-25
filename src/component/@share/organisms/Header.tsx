c02f8b5cfc617edbb66f76af83d3b3f9b0a3d6b9
mywhzy <cmj2393a@naver.com>
2023-07-25 15:03:03 +0900

    feat: Helper 클릭 시 Modal 상태 변경 경로 수정

--- src/component/@share/organisms/Header.tsx
+++ src/component/@share/organisms/Header.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Logo, Helper } from '@/component/@share/atom';
 import { useEffect, useState } from 'react';
 import { HelperModal } from './HelperModal';
@@ -7,15 +7,16 @@ import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
   const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const stepParams = searchParams.get('step');
   const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
   const [step, setStep] = useState<0 | 1 | 2 | 3>(1);
 
   useEffect(() => {
-    if (location.pathname === '/' || location.pathname === '/select-date')
-      setStep(1);
-    else if (location.pathname === '/select-time') setStep(2);
-    else if (location.pathname === '/login') setStep(3);
+    if (location.pathname === '/' || stepParams === '1') setStep(1);
+    else if (stepParams === '2') setStep(2);
+    else if (stepParams === '3') setStep(3);
     else setStep(0);
   }, [location]);
 
