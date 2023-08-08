4ed57d36520e5a19e93be5b4718fce2fd3bb4b78
Sujin-Hasse <2017104858@khu.ac.kr>
2023-08-08 23:41:31 +0900

    feat: 푸터 이용약관 및 개인정보처리방침 링크 연결

--- src/component/Footer/atoms/Privacy.tsx
+++ src/component/Footer/atoms/Privacy.tsx
@@ -5,11 +5,29 @@ interface PrivacyProps {
   secondContent: string;
 }
 
+const privacyLink =
+  'https://www.notion.so/likelion-11th/9122afea432645408dafbc37e334d32a?pvs=4';
+
+const termsOfUseLink =
+  'https://www.notion.so/likelion-11th/9122afea432645408dafbc37e334d32a?pvs=4';
+
 export const Privacy = ({ firstContent, secondContent }: PrivacyProps) => {
   return (
     <Container>
-      <PrivacyContent>{firstContent}</PrivacyContent>
-      <PrivacyContent>{secondContent}</PrivacyContent>
+      <PrivacyContent
+        href={privacyLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {firstContent}
+      </PrivacyContent>
+      <PrivacyContent
+        href={termsOfUseLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {secondContent}
+      </PrivacyContent>
     </Container>
   );
 };
@@ -19,7 +37,7 @@ const Container = styled.div`
   gap: 16px;
 `;
 
-const PrivacyContent = styled.button`
+const PrivacyContent = styled.a`
   ${({ theme }) => theme.typographies.body4};
   color: ${({ theme }) => theme.colors.gray2};
   &:hover {
