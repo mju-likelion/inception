1cf809b21e1db4250d34cd9d2e491fc1784410dc
Bhanjo <hanjo1515@naver.com>
2023-08-23 01:20:43 +0900

    Revert "feat: date 오름차순 정렬 메소드 분리"
    This reverts commit e983b2b5631ef9f517324e9881b20f88a76eaf70.

--- src/util/getDatesAsc.ts
+++
@@ -1,8 +0,0 @@
-export const getDatesAsc = (dateList: string[]) => {
-  return dateList.sort((a, b) => {
-    return a.localeCompare(b, undefined, {
-      numeric: true,
-      sensitivity: 'base',
-    });
-  });
-};
--- src/util/index.ts
+++ src/util/index.ts
@@ -4,4 +4,3 @@ export * from './getDateTypeToString';
 export * from './touchMoveDrag';
 export * from './getToastMessage';
 export * from './ScrollToTop';
-export * from './getDatesAsc';
