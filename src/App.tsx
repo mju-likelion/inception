cf76aed9d95ac2fe8664a959ee09f9a86c31be42
ndaemy <yuyaebean@gmail.com>
2023-08-28 23:29:54 +0900

    feat: App 에서 GA4 initialize 하기

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import { Header } from '@/component/@share';
 import { Footer } from '@/component/Footer';
 import { ScrollToTop } from '@/util';
 import { RecoilRoot } from 'recoil';
+import { useGA } from '@/hooks/useGA';
 
 /**@Note 모든 페이지에 적용할 설정 및 컴포넌트를 지정한다. */
 export function App() {
+  useGA();
+
   return (
     <RecoilRoot>
       <InitStyle />
---
+++ src/hooks/useGA.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
+export const useGA = () => {
+  useEffect(() => {
+    const gaId = process.env.REACT_APP_GA_ID;
+    const gaDebug = process.env.REACT_APP_GA_DEBUG;
+
+    if (!gaId) {
+      throw new Error('Google Analytics ID not found');
+    }
+
+    console.log('gaDebugMode', gaDebug);
+
+    ReactGA.initialize(gaId, { gaOptions: { debug: gaDebug === 'true' } });
+  }, []);
+};
